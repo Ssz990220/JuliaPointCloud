bd9ac2754ab12fdc4c729bf7cc4835d1bd1e565c
Ssz990220 <Ssz990220@sjtu.edu.cn>
2023-03-29 16:31:09 +0800

    Update Main function

--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
-include("FRICP.jl")
-using .FRICP
+include("JPC.jl")
+using .JPC
+using ProfileView
 begin
 	PC, N = load_PC("./Assets/source.ply")
 	source = PC;
@@ -11,7 +12,7 @@ end;
 
 begin 
 	function params(T)
-		max_iter = 100;
+		max_iter = 400;
 		f = "welsch"
 		aa = (νₛ = T(3.0), νₑ = T(1.0/(3.0*sqrt(3.0))), m = 5, d = 6, νₜₕ=T(1e-6),α=T(0.5))
 		return (max_iter = max_iter, f = f, aa = aa, stop = T(1e-5))
@@ -19,4 +20,10 @@ begin
 	par = params(eltype(source[1]))
 end
 
-FICP_P2P(source,target,par)
+T = FICP_P2P(target,source,par)
+
+fig,axis,_,obj = visualize(target)
+R,t = T2rt(inv(T))
+
+visualize!(axis, obj, transform_PC(source,R,t))
+fig
